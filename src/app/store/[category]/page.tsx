a5124e48c42a3bd7d24791e10fc86a9b451fd85a
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-01-20 13:21:12 +0100

    ruta dinamica en Next.js

---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,11 @@
+interface CategoryProps {
+    params: {
+        category: string
+    }
+}
+export default function Category(props: CategoryProps){
+    const { category } = props.params;
+    return(
+        <h1>Categoría dinámica: { category }</h1>
+    )
+}
