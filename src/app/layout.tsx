d6e29cf630439e463010ab43ec45eb3f94a15378
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-01-28 16:44:57 +0100

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import { Roboto, Geist, Geist_Mono } from "next/font/google";
+import { Hero } from "app/components/home/Hero";
+import { Description } from "app/components/home/Description";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import 'app/sass/globals.sass';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const robot = Roboto({
   weight: ["100", "300", "400", "500", "700"],
@@ -28,6 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={robot.className}>
         <Header />
+        <Hero />
+        <Description />
         {children}
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { Description } from "app/components/shared/home/Description";
-import { Hero } from "app/components/shared/home/Hero";
-import { MainProducts } from "app/components/shared/home/MainProducts";
+import { MainProducts } from "app/components/home/MainProducts";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <Description />
       <MainProducts />
     </main>
   );
