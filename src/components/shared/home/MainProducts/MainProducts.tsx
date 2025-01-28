706811fff0c8208a41783403f8d2362bae632c04
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-01-28 16:18:21 +0100

    Consumir una API

--- src/components/shared/home/MainProducts/MainProducts.tsx
+++ src/components/shared/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,17 @@
-export const MainProducts = () => {
+const getProducts = async () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-01/products.json`, {
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
+        })
+    })
+
+    const data = await response.json();
+    return data;
+}
+
+export const MainProducts = async () => {
+    const products = await getProducts();
+    console.log(products);
     return(
         <section>
             <h1>MainProducts</h1>
