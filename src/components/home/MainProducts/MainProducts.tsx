73d4886862c121793835492c3e80c8f3349ca1ad
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-10 15:02:36 +0100

    ✨ Arquitectura profesional para data fetching

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,22 +1,7 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
-import { error } from 'console'
-
-const getProducts = async () => {
-    try {
-
-        const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-01/products.json`, {
-            headers: new Headers({
-                'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
-            })
-        })
-        const { products } = await response.json();
-        return products;
-
-    } catch(error) {
-      throw new Error('Error');
-    }
-}
+import { error } from 'console';
+import { getProducts } from 'app/services/shopify';
 
 export const MainProducts = async () => {
     const products = await getProducts()
