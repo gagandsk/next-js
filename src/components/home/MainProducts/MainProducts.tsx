e88037f3a5d62647580b97a5d016e29ba4570954
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-10 15:14:01 +0100

    Arquitectura 'Back For Frontend'

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -4,8 +4,10 @@ import { error } from 'console';
 import { getProducts } from 'app/services/shopify';
 
 export const MainProducts = async () => {
-    const products = await getProducts()
-    
+    //const products = await getProducts()
+    const response = await fetch('http://localhost:3000/api');
+    const { products } = await response.json();
+
     return (
       <section className={styles.MainProducts}>
         <h3>✨ New products released!</h3>
