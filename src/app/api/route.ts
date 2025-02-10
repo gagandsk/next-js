e2d2b5b780e5e2596ad4eb1ae19e13733b2c3d4c
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-10 15:09:23 +0100

    Backend: manejando rutas con archivos Route Handlers

---
+++ src/app/api/route.ts
@@ -0,0 +1,28 @@
+import { getProducts } from "app/services/shopify";
+
+export async function GET(){
+    const products = await getProducts();
+    return Response.json({ products })
+}
+
+/*
+export async function POST(){
+    const message = "POST";
+    return Response.json({ message })
+}
+
+export async function DELETE(){
+    const message = "DELETE";
+    return Response.json({ message })
+}
+
+export async function PUT(){
+    const message = "PUT";
+    return Response.json({ message })
+}
+
+export async function PATCH(){
+    const message = "PATCH";
+    return Response.json({ message })
+}
+*/
