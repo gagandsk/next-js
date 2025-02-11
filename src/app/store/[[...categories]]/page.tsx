76578bf9a9b07dffef4fd6f20f3557d3e398917e
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-11 13:21:43 +0100

    Patrones de data fetching

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductsWrapper } from "app/components/Store/ProductWrapper";
-import { getProducts } from "app/services/shopify";
+import { getCollections } from "app/services/shopify/collections";
+import { getProducts } from "app/services/shopify/products";
 
 interface CategoryProps {
     params: {
@@ -9,6 +10,7 @@ interface CategoryProps {
 }
 export default async function Category(props: CategoryProps){
     const products = await getProducts();
+    
     return(
         <ProductsWrapper products={products}/>
     )
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
 import { error } from 'console';
-import { getProducts } from 'app/services/shopify';
+import { getProducts } from 'app/services/shopify/products';
 
 export const MainProducts = async () => {
     //const products = await getProducts()
