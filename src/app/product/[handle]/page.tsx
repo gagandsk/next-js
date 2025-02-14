ec736e3a24f040e9a2ff42a046a57cabfcc00086
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-14 19:38:31 +0100

    ✨ SearchParams

---
+++ src/app/product/[handle]/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { useParams, useSearchParams } from "next/navigation"
+
+interface ProductPageProps {
+    searchParams: {
+        id: string
+    }
+}
+
+export default function ProductPage(){
+    const params = useParams()
+    const searchParams = useSearchParams()
+    const id = searchParams.get("id")
+    console.log('searchParams', id)
+    return <h1>Product Page</h1>
+  }
