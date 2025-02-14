142175d8cb51313e8cc33a0f9f4ef04d28901c46
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-14 20:01:42 +0100

    Pagina de producto

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { useParams, useSearchParams } from "next/navigation"
+import { ProductView } from "app/components/product"
+import { getProducts } from "app/services/shopify/products";
 
 interface ProductPageProps {
     searchParams: {
@@ -7,10 +7,11 @@ interface ProductPageProps {
     }
 }
 
-export default function ProductPage(){
-    const params = useParams()
-    const searchParams = useSearchParams()
-    const id = searchParams.get("id")
-    console.log('searchParams', id)
-    return <h1>Product Page</h1>
+export default async function ProductPage({ searchParams }: ProductPageProps) {
+    const id = searchParams.id
+    const products = await getProducts(id);
+    const product = products[0];
+    console.log(product)
+  
+    return <ProductView product={product} />
   }
